422d000cf4d2781f6b2150af7ad699415c2ecf43
Ryan Flanagan <flanagan.ryan@gmail.com>
2021-02-09 11:46:21 -0500

    made edits requested by Jeff

---
+++ app.js
@@ -0,0 +1,13 @@
+// var message = "";
+
+// $("#notifyform").on("submit", function () {
+//   message = $("#contactform").serialize();
+//   $.ajax({
+//     url: "//formspree.io/flanagan.ryan@gmail.com",
+//     method: "POST",
+//     data: { message: message },
+//     dataType: "json",
+//   });
+//   alert("Thanks for the email, we'll be in touch promptly.");
+//   return false;
+// });
